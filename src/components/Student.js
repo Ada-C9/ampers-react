1cfc0af67707cc4e36f74c25293d85f0e4d8fe37
CheezItMan <chris@adadevelopersacademy.org>
2018-05-30 11:01:29 -0700

    Added event handlers

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 import './Student.css';
 
 class Student extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPresent: true,
+      name: props.name,
+    };
+  }
+
+
 
   // static propTypes = {
   //   name: PropTypes.string.isRequired,
@@ -22,20 +32,54 @@ class Student extends Component {
     return 'student';
   }
 
+  onToggleButton = () => {
+    const isPresent = !this.state.isPresent
+    this.setState({
+      isPresent,
+    });
+
+  }
+
+  onNameChange = (event) => {
+    console.log(event.target.value);
+    console.log(this.props.name);
+    // const name = event.target.value;
+    // this.setState({
+    //   name,
+    // });
+
+    this.setState({
+      name: event.target.value,
+    })
+  }
+
   render() {
     // const studentStyle = {
     //   color: 'purple',
     //   fontSize: '20pt',
     // };
-
     const studentAge = this.props.studentData.age;
     const studentEmail = this.props.email;
     return (
       <article
         className={ this.isTopStudent() }>
-        <h3>{this.props.name}</h3>
+        <h3>
+          {this.state.name}
+        </h3>
+        <input onChange={ this.onNameChange }
+          type="text"
+          name="name"
+        />
+        <h4>
+          {this.state.isPresent ? "In class": "Absent"}
+        </h4>
         <p>Age: {studentAge}</p>
         <p>email: {studentEmail}</p>
+        <button
+          onClick={ this.onToggleButton }>
+          Toggle Present
+        </button>
+
       </article>
     );
   }
